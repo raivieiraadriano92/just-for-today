427943cdc6cb34f4fc28c7bafebaaee5e7d36711
Raí Adriano <raivieiraadriano92@gmail.com>
2025-06-17 16:43:21 -0300

    Remove unused font loading logic and delete SpaceMono font file for cleaner codebase

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
@@ -12,14 +11,10 @@ import "react-native-reanimated";
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  // if (!loaded) {
+  //   // Async font loading only occurs in development.
+  //   return null;
+  // }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
