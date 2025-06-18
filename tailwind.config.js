2f1535affa33401ae7988ada9802613498f8910f
Raí Adriano <raivieiraadriano92@gmail.com>
2025-06-18 09:27:17 -0300

    feat: add custom color variables for Tailwind CSS theming and enable dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  darkMode: "class",
   content: ["./{app,components}/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--background),1)",
+        border: "rgba(var(--border),1)",
+        card: "rgba(var(--card),1)",
+        notification: "rgba(var(--notification),1)",
+        primary: "rgba(var(--primary),1)",
+        text: "rgba(var(--text),1)",
+      },
+    },
   },
   plugins: [],
 };
